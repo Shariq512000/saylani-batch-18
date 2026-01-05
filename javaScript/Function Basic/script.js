a302967460f7febcbe4372ab7b7ff82a211fc46d
Shariq Siddiqui <99816206+Shariq512000@users.noreply.github.com>
2026-01-05 10:45:28 +0500

    get value from html and set value on html

--- javaScript/Function Basic/script.js
+++ javaScript/Function Basic/script.js
@@ -1,7 +1,37 @@
-
 function getData(){
     let userName = document.getElementById("userName").value;
     let rollNumber = document.getElementById("rollNumber").value
-
     console.log(userName, rollNumber)
+    // alert("Function Run")
+
+    // let abc = "value";
+    // abc = "new values"
+
+    
+    // document.getElementById("result").innerHTML = "<p>" + userName + "(" + rollNumber + ")" + "</p>";
+
+
+    document.getElementById("result").innerHTML = `<p>${userName} <span>(${rollNumber})</span></p>`;
+
+
+    // console.log(resultElement);
 }
+
+
+
+
+
+
+
+// let
+// var
+// const
+
+// var firstVariable = "45";
+// firstVariable = "newValue"
+// var firstVariable = "65"
+
+// let secondVariable = "assign value";
+// secondVariable = "New Value"
+
+// const thirdVariable = "Assigned Value";
