054dd96e918d16af93c4096ed89b504c61eaad50
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2026-02-16 11:01:15 +0500

    dom basic

---
+++ javaScript/dom-revision/script.js
@@ -0,0 +1,2 @@
+let p = document.childNodes[1].childNodes[1].childNodes[0].childNodes[0]
+console.log(p.nextSibling.previousSibling.childNodes[0].nodeValue)
---
+++ javaScript/dom/app.js
@@ -0,0 +1,89 @@
+console.log("JavaScript Connected!");
+
+
+// let p1 = document.getElementById("p1");
+
+// // console.log(p1);
+
+// // console.log(p1.parentNode);
+
+// console.log(p1.nextSibling);
+
+// console.log(p1.nextElementSibling);
+
+
+// let list = document.getElementById("list");
+
+// console.log(list);
+
+// console.log(list.childNodes.length);
+// console.log(list.childNodes);
+// console.log(list.children.length);
+// console.log(list.children);
+
+
+// let wrap = document.getElementById("wrap");
+
+// console.log(wrap.firstChild);
+// console.log(wrap.firstElementChild);
+
+
+// let wrap = document.getElementById("wrap");
+
+
+
+// console.log(wrap.lastChild);
+// console.log(wrap.lastElementChild.previousElementSibling);
+
+
+// let p = document.getElementById("p");
+
+// console.log(p.childNodes);
+
+// // console.log();
+
+// let em = p.querySelector("em");
+
+// console.log(em.firstChild.nodeValue);
+
+
+// let inner = document.getElementById("inner");
+
+// console.log(inner.parentNode);
+// console.log(inner.parentElement);
+
+// let html = document.documentElement;
+
+// console.log(html.parentNode);
+// console.log(html.parentElement);
+
+
+// console.log(document.childNodes[1].childNodes[2].children[0].children[0]);
+
+
+// var p = document.getElementsByTagName("p");
+
+// let contents = p[2].innerHTML;
+
+// console.log(contents);
+
+// console.log(p);
+
+
+// let cal = document.getElementById("cal");
+
+// let p = cal.getElementsByTagName("p");
+
+// let content = p[1].innerHTML;
+
+// console.log(content);
+// console.log(p);
+
+// let box = document.getElementById("box");
+
+
+// for(var i = 0 ;i < box.childNodes.length; i++){
+//     console.log(box.childNodes[i].nodeType);
+    
+// }
+
