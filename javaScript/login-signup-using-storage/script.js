7ddff92ed33b7f8b23102bcf8e83e8c37dc188d4
Meer Quais <meerquais017@gmail.com>
2026-03-06 10:04:36 +0500

    localstorage

--- javaScript/login-signup-using-storage/script.js
+++ javaScript/login-signup-using-storage/script.js
@@ -1,6 +1,6 @@
-let localStrVal = localStorage.getItem("users") // 2 // null
+let localStrVal = localStorage.getItem("users") // null
 
-let users = [] // 2 []
+let users = []
 
 if(localStrVal){
     users = JSON.parse(localStrVal)
@@ -11,21 +11,62 @@ function registerUser(){
     let userEmail = document.getElementById("userEmail").value
     let userPassword = document.getElementById("userPassword").value
 
+    // for(let i = 0; i < users.length; i++){
+    //     if(users[i].email == userEmail){
+    //         alert("Already Register Account on this Email")
+    //         return;
+    //     }
+    // }
+
+    let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    if(regex.test(userPassword)){
+
+        let userObj = {
+            name: userName,
+            email: userEmail,
+            password: userPassword
+        }
+        users.push(userObj)
+    
+        let convertUserToStr = JSON.stringify(users)
+    
+        localStorage.setItem("users", convertUserToStr)
+    }else{
+        document.getElementById("helperText").innerText = "Weak Password, Try Strong Password For Signup! ex. Password must contains a upparcase, a lowercase, a number, special character and atleast 8 digit"
+    }
+
+}
+
+console.log(users)
+
+console.log(window.location.pathname)
+
+function loginUser(){
+    let userEmail = document.getElementById("userEmail").value;
+    let userPassword = document.getElementById("userPassword").value;
+
+    let currentUser = null;
+
     for(let i = 0; i < users.length; i++){
         if(users[i].email == userEmail){
-            alert("Already Register Account on this Email")
-            return;
+            currentUser = users[i];
+            break;
         }
     }
 
-    let userObj = {
-        name: userName,
-        email: userEmail,
-        password: userPassword
+    if(currentUser){
+
+        if(currentUser.password == userPassword){
+            // alert("User Logedin")
+            // ---------> login-signup-test ---> Repository Name
+            // ---------> dashboard.html ----> Page Name
+            window.location.pathname = "/login-signup-test/dashboard.html"
+        }else{
+            alert("Password did not matched")
+        }
+
+    }else{
+        alert("Account Not Register with this email")
     }
-    users.push(userObj)
-
-    let convertUserToStr = JSON.stringify(users)
-
-    localStorage.setItem("users", convertUserToStr)
 }
