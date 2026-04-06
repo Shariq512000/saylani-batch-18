7898ef8eaca658145cbc169e9bddb2788793e044
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2026-04-06 11:05:16 +0500

    some advance concept

--- javaScript/if-else/script.js
+++ javaScript/if-else/script.js
@@ -6,20 +6,34 @@
 //     alert("You are not Adult")
 // }
                     // "600"
-// let totalMark = prompt("Enter Your Total Marks")
-//                     // "450"
-// let obtainedMark = prompt("Enter Your Obtained Marks")
-//                     // 600
-// let totalMarkInNum = Number(totalMark)
-//                     // 450
-// let obtainedMarkInNumber = Number(obtainedMark)
+let totalMark = prompt("Enter Your Total Marks")
+                    // "450"
+let obtainedMark = prompt("Enter Your Obtained Marks")
+                    // 600
+let totalMarkInNum = Number(totalMark)
+                    // 450
+let obtainedMarkInNumber = Number(obtainedMark)
 
-// let percentage = obtainedMarkInNumber / totalMarkInNum * 100;
+let percentage = obtainedMarkInNumber / totalMarkInNum * 100;
 
-// console.log(percentage)
+console.log(percentage)
 
-// if(percentage >= 33){
-//     alert("Passed")
+let grade = (percentage >= 80) ? "A+" : (percentage >= 70) ? "A" : (percentage >= 60) ? "B" : (percentage >= 50) ? "C" : (percentage >= 40) ? "D" : (percentage >= 33) ? "E" : "F"
+
+alert(grade)
+
+// if(percentage >= 80){
+//     alert("A+")
+// }else if(percentage >= 70){
+//     alert("A")
+// }else if(percentage >= 60){
+//     alert("B")
+// }else if(percentage >= 50){
+//     alert("C")
+// }else if(percentage >= 40){
+//     alert("D")
+// }else if(percentage >= 33){
+//     alert("E")
 // }else{
 //     alert("Failed")
 // }
@@ -31,14 +45,14 @@
 
 
 
-let age = 25
+// let age = 25
 
-if(age <= 18){
-    alert("First Block")
-}else if(age == 25){
-    alert("Second Block")
-}else if(age <= 25){
-    alert("Third Block")
-}else{
-    alert("Else Block")
-}
+// if(age <= 18){
+//     alert("First Block")
+// }else if(age == 25){
+//     alert("Second Block")
+// }else if(age <= 25){
+//     alert("Third Block")
+// }else{
+//     alert("Else Block")
+// }
