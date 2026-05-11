4b740ed44dada5252eed73180e456eb0a36e83c2
Shariq Siddiqui <99816206+Shariq512000@users.noreply.github.com>
2026-05-11 10:32:33 +0500

    react-routing

--- react/maps-and-api-calls/src/App.jsx
+++ react/maps-and-api-calls/src/App.jsx
@@ -1,25 +1,23 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import axios from "axios";
-import "./App.css"
-import ProductCard from './component/ProductCard';
-import Card from './component/card';
+import "./App.css";
+import Card from "./component/card";
 
 function App() {
-
   const [products, setProducts] = useState([]);
 
-  const getApiData = async() => {
+  const getApiData = async () => {
     try {
       const apiRes = await axios.get("https://dummyjson.com/products"); // intezaar krta hai response aane ka
-      setProducts(apiRes.data.products)
+      setProducts(apiRes.data.products);
     } catch (error) {
-      console.log("Err", error)
+      console.log("Err", error);
     }
-  }
+  };
 
   useEffect(() => {
-    getApiData()
-  }, [])
+    getApiData();
+  }, []);
 
   // console.log("Test")
 
@@ -32,21 +30,17 @@ function App() {
   //   test()
   // } , [])
 
-
   return (
     <div className="App">
       {products.map((eachProduct) => {
         // console.log(eachProduct)
-        return(
-          <Card key={eachProduct.id} product={eachProduct} />
-        )
+        return <Card key={eachProduct.id} product={eachProduct} />;
       })}
     </div>
   );
 }
 
 export default App;
-
 
 // Mount
 // Update
