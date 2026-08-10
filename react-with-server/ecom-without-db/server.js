b9b39b0562a5b8ec236d3f86f8c0b68ae8c48ad4
Meer Quais <meerquais017@gmail.com>
2026-08-10 10:44:02 +0500

    add, update, and get product apis

--- react-with-server/ecom-without-db/server.js
+++ react-with-server/ecom-without-db/server.js
@@ -5,10 +5,14 @@ const PORT = 5000;
 
 let products = [] // TODO Replace with Database
 
+// app.get('/', (req, res) => {
+//     res.send("Hello World")
+// }) //url, function
+
+// get --> get
 // add --> Post
 // edit --> put, patch
 // delete --> delete
-// get --> get
 
 // {}
 
@@ -26,8 +30,47 @@ app.use(express.json());
 
 app.post('/add-product', (req, res) => {
     const productBody = req.body;
-    products.push(productBody);
+    if (!productBody?.title || !productBody?.price || !productBody?.description) {
+        res.status(400).send({ status: "error", message: "Required Parameter Missing" })
+        return;
+    }
+    products.push({ id: new Date().getTime(), ...productBody });
+    console.log(products)
     res.status(201).send({ status: "success", message: "Product Added Successfully" })
+})
+
+// query parameter --> optional
+// url parameter   --> required
+
+//  /edit-product
+
+// [
+//     {id: 1, ...},
+//     {id: 2, ...},
+//     {id: 3, ...},
+//     {id: 4, ...},
+//     {id: 5, ...},
+// ]
+// /edit-product/33
+app.put('/edit-product/:id', (req, res) => {
+    const productId = req.params.id;
+    let targetedProduct = products.find((eachProduct) => eachProduct.id == productId)
+    if (!targetedProduct) {
+        res.status(404).send({ status: "error", message: `Product Not Found with id ${productId}` })
+        return;
+    }
+    const productBody = req.body;
+    if (!productBody?.title || !productBody?.price || !productBody?.description) {
+        res.status(400).send({ status: "error", message: "Required Parameter Missing" })
+        return;
+    }
+    products = products.filter((eachProduct) => eachProduct.id != productId)
+    products.push({ id: targetedProduct.id, title: productBody?.title, price: productBody?.price, description: productBody?.description })
+    res.status(200).send({ status: "success", message: "Product Update Successfully" })
+})
+
+app.get('/get-all-products', (req, res) => {
+    res.status(200).send({ status: "success", products })
 })
 
 app.listen(PORT, () => {
