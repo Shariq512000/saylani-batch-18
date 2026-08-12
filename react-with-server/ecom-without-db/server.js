94d1bd1c5f04b989285988ff73d230a5964a92ea
Muhammad Umair <99816206+Shariq512000@users.noreply.github.com>
2026-08-12 10:45:58 +0500

    Dummy Ecommerce

--- react-with-server/ecom-without-db/server.js
+++ react-with-server/ecom-without-db/server.js
@@ -1,76 +1,60 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 5000;
 
 let products = [] // TODO Replace with Database
 
-// app.get('/', (req, res) => {
-//     res.send("Hello World")
-// }) //url, function
-
-// get --> get
-// add --> Post
-// edit --> put, patch
-// delete --> delete
-
-// {}
-
-// const apiImagination = {
-//     method: "post",
-//     url: "/add-product",
-//     body: {
-//         "title": "Smart Watch",
-//         "price": 50,
-//         "description": "Test Api Smart Watch"
-//     }
-// }
-
+app.use(cors());
 app.use(express.json());
+
+app.get('/get-all-products', (req, res) => {
+    res.send({ status: "success", products })
+})
 
 app.post('/add-product', (req, res) => {
     const productBody = req.body;
-    if (!productBody?.title || !productBody?.price || !productBody?.description) {
-        res.status(400).send({ status: "error", message: "Required Parameter Missing" })
+    if (!productBody?.title || !productBody?.price || !productBody?.description || !productBody?.image) {
+        res.send({ status: "error", message: "Required Parameter Missing" })
         return;
     }
     products.push({ id: new Date().getTime(), ...productBody });
     console.log(products)
-    res.status(201).send({ status: "success", message: "Product Added Successfully" })
+    res.send({ status: "success", message: "Product Added Successfully" })
 })
 
-// query parameter --> optional
-// url parameter   --> required
-
-//  /edit-product
-
-// [
-//     {id: 1, ...},
-//     {id: 2, ...},
-//     {id: 3, ...},
-//     {id: 4, ...},
-//     {id: 5, ...},
-// ]
-// /edit-product/33
 app.put('/edit-product/:id', (req, res) => {
     const productId = req.params.id;
-    let targetedProduct = products.find((eachProduct) => eachProduct.id == productId)
-    if (!targetedProduct) {
-        res.status(404).send({ status: "error", message: `Product Not Found with id ${productId}` })
+    let targetedProductId = null;
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id == productId) {
+            targetedProductId = i
+        }
+    }
+    // let targetedProduct = products.find((eachProduct) => eachProduct.id == productId)
+    if (targetedProductId == null) {
+        res.send({ status: "error", message: `Product Not Found with id ${productId}` })
         return;
     }
     const productBody = req.body;
-    if (!productBody?.title || !productBody?.price || !productBody?.description) {
-        res.status(400).send({ status: "error", message: "Required Parameter Missing" })
+    if (!productBody?.title || !productBody?.price || !productBody?.description || !productBody?.image) {
+        res.send({ status: "error", message: "Required Parameter Missing" })
         return;
     }
-    products = products.filter((eachProduct) => eachProduct.id != productId)
-    products.push({ id: targetedProduct.id, title: productBody?.title, price: productBody?.price, description: productBody?.description })
+    // products = products.filter((eachProduct) => eachProduct.id != productId)
+    // products.push({ id: targetedProduct.id, title: productBody?.title, price: productBody?.price, description: productBody?.description })
+    products[targetedProductId].title == productBody?.title
+    products[targetedProductId].price == productBody?.price
+    products[targetedProductId].description == productBody?.description
+    products[targetedProductId].image == productBody?.image
     res.status(200).send({ status: "success", message: "Product Update Successfully" })
 })
 
-app.get('/get-all-products', (req, res) => {
-    res.status(200).send({ status: "success", products })
+app.delete('/delete-product/:id', (req, res) => {
+    let targetedProduct = req.params.id;
+    products = products.filter((eachItem) => eachItem.id != targetedProduct);
+    res.send({ status: "success", message: "product deleted successfully" })
 })
 
 app.listen(PORT, () => {
---
+++ react-with-server/ecom-without-db/web/src/App.jsx
@@ -0,0 +1,104 @@
+// import logo from './logo.svg';
+import { useEffect, useState } from "react";
+import { useFormik } from "formik";
+import axios from "axios";
+import './App.css';
+
+function App() {
+
+  const [allProducts, setAllProducts] = useState([])
+
+  const getAllProducts = async () => {
+    try {
+      const apiRes = await axios.get("http://localhost:5000/get-all-products");
+      // console.log("apiRes", apiRes.data)
+      setAllProducts(apiRes.data.products)
+    } catch (error) {
+      console.log("Error", error)
+    }
+  }
+
+  useEffect(() => {
+    getAllProducts()
+  }, [])
+
+  const formik = useFormik({
+    initialValues: {
+      productImage: '',
+      title: '',
+      price: 0,
+      description: '',
+    },
+    onSubmit: async (values) => {
+      // console.log(values)
+      try {
+        const apiRes = await axios.post("http://localhost:5000/add-product", {
+          title: values.title,
+          price: values.price,
+          description: values.description,
+          image: values.productImage
+        })
+        // console.log("apiRes", apiRes.data)
+        getAllProducts()
+      } catch (error) {
+        console.log("Err", error)
+      }
+    }
+  })
+
+  return (
+    <div className="App">
+
+      <form onSubmit={formik.handleSubmit}>
+        <label>
+          Image Url: <input type="url" placeholder="https://example/1.png" name="productImage" onChange={formik.handleChange} value={formik.values.productImage} />
+        </label>
+        <br />
+        <label>
+          Title:
+          <input
+            type="text"
+            placeholder="Mobile"
+            name="title"
+            onChange={formik.handleChange}
+            value={formik.values.title}
+          />
+        </label>
+        <br />
+        <label>
+          price:
+          <input
+            type="number"
+            placeholder="5000"
+            name="price"
+            onChange={formik.handleChange}
+            value={formik.values.price}
+          />
+        </label>
+        <br />
+        <label>
+          Description:
+          <textarea
+            name="description"
+            onChange={formik.handleChange}
+            value={formik.values.description}
+          ></textarea>
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+
+      <div>
+        {allProducts.map((eachProduct) =>
+          <div key={eachProduct.id}>
+            <h1>Title: {eachProduct.title}</h1>
+            <h4>{eachProduct.price}</h4>
+            <p>{eachProduct.description}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
