75c62f5842dedc9e9b5aa2bc650515c65e80f717
Meer Quais <meerquais017@gmail.com>
2026-09-07 09:14:20 +0500

    some changes

--- react-with-server/ecom-postgreSQL/server.js
+++ react-with-server/ecom-postgreSQL/server.js
@@ -68,10 +68,46 @@ app.post("/student", async (req, res) => {
 
 app.get("/students", async (req, res) => {
     try {
-        const students = await db.query(`SELECT * from students`);
-        // console.log("Students", students.rows)
+        const students = await db.query(`SELECT * from students;`);
+        // const students = await db.query(`SELECT id AS student_id, first_name || ' ' || last_name AS full_name from students;`);
+        // const students = await db.query(`SELECT * from students WHERE course = 'Python'`);LIMIT 20
+        // const students = await db.query(`SELECT * from students ORDER BY first_name`);
+        // const students = await db.query(`SELECT * from students LIMIT 20 OFFSET 20`);
+        // const students = await db.query(`SELECT MIN(age) FROM students;`);
+        // console.log("Students", students)
         res.status(200).send({ status: "success", students: students.rows });
     } catch (error) {
+        res.status(500).send({ status: "error", message: "Internal Server Error" })
+    }
+})
+
+app.put("/student/:id", async (req, res) => {
+    const studentId = req.params.id;
+    // firstName, lastName, course, batch, rollNumber, age
+    const reqBody = req.body;
+    if (!reqBody.firstName || !reqBody.lastName || !reqBody.course || !reqBody.batch || !reqBody.rollNumber || !reqBody.age) {
+        res.status(400).send({ status: "error", message: "Required Parameter Missing" })
+        return;
+    }
+
+    try {
+        const dbRes = await db.query(`UPDATE students SET first_name = $1, last_name = $2, course = $3, batch = $4, roll_number = $5, age = $6 WHERE id = ${studentId};`,
+            [reqBody.firstName, reqBody.lastName, reqBody.course, reqBody.batch, reqBody.rollNumber, reqBody.age])
+        res.status(201).send({ status: "success", message: "Student Added Successfully" })
+    } catch (error) {
+        console.log("Err", error);
+        res.status(500).send({ status: "error", message: "Internal Server Error" })
+    }
+})
+
+app.delete("/student/:id", async (req, res) => {
+    const studentId = req.params.id;
+
+    try {
+        const dbRes = await db.query(`DELETE FROM students WHERE id = ${studentId};`)
+        res.status(201).send({ status: "success", message: "Student Added Successfully" })
+    } catch (error) {
+        console.log("Err", error);
         res.status(500).send({ status: "error", message: "Internal Server Error" })
     }
 })
